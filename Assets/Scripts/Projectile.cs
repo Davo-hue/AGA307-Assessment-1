2eb8567a51ef7d7ca28cf009df2d2903e04c26ae
Davo-hue <dnowland@myjmc.edu.au>
2024-06-28 10:05:37 +1000

    Target

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,4 +15,19 @@ public class Projectile : MonoBehaviour
         Destroy(this.gameObject);
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Target"))
+        {
+            //change color when hit
+            collision.gameObject.GetComponentInChildren<Renderer>().material.color = Color.red;
+            //destroy after 1 second
+            Destroy(collision.collider.gameObject, 1f);
+            //destroy object
+            Destroy(this.gameObject);
+        }
+
+       
+    }
+
 }
